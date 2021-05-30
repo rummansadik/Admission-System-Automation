3a3b942d5b5e2ad54c7ca505b4c7ebb222abb310
Rumman <rummansadik@gmail.com>
2021-05-30 08:59:48 +0600

    Created logout path

--- Project/urls.py
+++ Project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from login_system.views import home
 
@@ -10,6 +10,9 @@ urlpatterns = [
     path('login/student',
          LoginView.as_view(template_name='student/login.html'),
          name='student_login'),
+    path('logout/student',
+         LogoutView.as_view(template_name='student/logout.html'),
+         name='student_logout'),
     path('login/teacher',
          LoginView.as_view(template_name='teacher/login.html'),
          name='teacher_login'),
