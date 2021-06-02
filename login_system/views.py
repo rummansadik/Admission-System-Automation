703094a562a492823a5143b020afa223181f1fcc
Md Turin <matkhan01@yahoo.com>
2021-06-03 00:46:31 +0600

    forms & views modified

--- login_system/views.py
+++ login_system/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import StudentRegisterForm, TeacherRegisterForm
 
 
 def home(request):
@@ -9,27 +9,27 @@ def home(request):
 
 def student_register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = StudentRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('home_page')
     else:
-        form = UserRegisterForm()
+        form = StudentRegisterForm()
     return render(request, 'student/register.html', {'form': form})
 
 
 def teacher_register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = TeacherRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('home_page')
     else:
-        form = UserRegisterForm()
+        form = TeacherRegisterForm()
     return render(request, 'teacher/register.html', {'form': form})
 
 
