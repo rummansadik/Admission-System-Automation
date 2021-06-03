041f4a19065ab377f6f7ab9d8f63a52e946bbd44
Md Turin <matkhan01@yahoo.com>
2021-06-03 06:51:58 +0600

    setting changes for deployment

--- Project/settings.py
+++ Project/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$r^cv#c4m4jag$cjhb1kv7=0y=uoi9)+!_yc-f3*au7r5c%9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'admission-automation-system.herokuapp.com'
+]
 
 
 # Application definition
@@ -44,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
